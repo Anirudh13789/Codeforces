5c83306097a4f773a66f84951d53e007b07045a6
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-08-09 20:34:08 +0530

    solved daily prb

---
+++ 230A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int s,n;
+    cin>>s>>n;
+    pair<int,int> dragons[1005];
+
+    for(int i=0;i<n;i++){
+        cin>>dragons[i].first>>dragons[i].second;
+    }
+    sort(dragons,dragons+n);
+    for(int i=0;i<n;i++){
+        if(s<=dragons[i].first){
+            cout<<"NO";
+            return 0;
+        }
+        s+=dragons[i].second;
+    }
+    cout<<"YES";
+    return 0;
+}
