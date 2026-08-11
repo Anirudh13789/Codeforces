931ef384fede69a2ebc565ef6a9dc2e45a27da55
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-08-11 14:02:14 +0530

    daily prb solved

---
+++ 1703A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        for (char &ch : s){
+            ch=toupper(ch);
+        }
+        if(s=="YES"){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
