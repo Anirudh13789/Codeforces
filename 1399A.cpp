1ad6edf85143171d7079a9d89bf433efeb0dc401
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-08 08:59:24 +0530

    daily prb solved

---
+++ 1399A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(a[i]-a[i-1]>1){
+                cnt++;
+            }
+        }
+        if(cnt==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+
+}
