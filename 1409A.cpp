8f6e11e03293fd3fa9904a8d3f949ab5bdba2df2
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-12 09:42:01 +0530

    solved daily prb

---
+++ 1409A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int diff=abs(b-a);
+        if(diff%10!=0){
+            cout<<(diff/10)+1<<endl;
+        }
+        else{
+            cout<<diff/10<<endl;
+        }
+    }
+    return 0;
+}
