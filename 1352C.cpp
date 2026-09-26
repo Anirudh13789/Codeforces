d31c66cc36e6bfa143ea1a2efde59267990525ff
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-26 12:24:03 +0530

    solved daily prb

---
+++ 1352C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        long long ans=k+(k-1)/(n-1);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
